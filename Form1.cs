9912bf0c3a589dfe46409ffd895e33511e0d76b9
guaaan <juampablo.txt@gmail.com>
2021-07-09 17:19:05 -0400

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaterialSkin
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(
+                Primary.Indigo500,
+                Primary.Indigo700,
+                Primary.Indigo100,
+                Accent.Pink200,
+                TextShade.WHITE);
+            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.EnforceBackcolorOnAllComponents = true;
+            materialSkinManager.AddFormToManage(this);
+        }
+    }
+}
